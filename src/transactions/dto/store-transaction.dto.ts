e967f2ae0c503bf2cbab85b4733081cfef67904c
Daniel Andrade <dani.andrade@ua.pt>
2021-05-24 16:24:56 +0100

    :recycle: Added Decorators/Transforms

--- src/transactions/dto/store-transaction.dto.ts
+++ src/transactions/dto/store-transaction.dto.ts
@@ -1,7 +1,20 @@
+import { Transform } from 'class-transformer';
+import { IsDate, IsISO8601, IsNumber, IsString, Min } from 'class-validator';
+
 export class StoreTransactionDto {
+  @IsNumber()
   amount: number;
+
+  @IsNumber()
   pricePerKw: number;
+
+  @IsString()
   consumerId: string;
+
+  @IsString()
   prosumerId: string;
+
+  @IsISO8601()
+  @Transform(({ value }) => new Date(value))
   performedAt: Date;
 }
