e215a506deeee379418f23c790d3297cd04da40f
Daniel Andrade <dani.andrade@ua.pt>
2021-05-24 16:24:56 +0100

    :recycle: Added Decorators/Transforms

--- src/measures/dto/store-measure.dto.ts
+++ src/measures/dto/store-measure.dto.ts
@@ -1,4 +1,11 @@
+import { Transform } from 'class-transformer';
+import { IsISO8601, IsNumber } from 'class-validator';
+
 export class StoreMeasureDto {
+  @IsNumber()
   value: number;
+
+  @IsISO8601()
+  @Transform(({ value }) => new Date(value))
   measuredAt: Date;
 }
