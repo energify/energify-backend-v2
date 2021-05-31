74bab7fd6c7ea4945028b37f53ab0ae7824f0e02
Vasco Sousa <myemailvasco@gmail.com>
2021-05-31 16:00:40 +0100

    :recycle: ObjectId in IUser interface

--- src/auth/decorators/authed-user.decorator.ts
+++ src/auth/decorators/authed-user.decorator.ts
@@ -1,5 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Types } from 'mongoose';
 
 export const AuthedUser = createParamDecorator((_, ctx: ExecutionContext) => {
-  return ctx.switchToHttp().getRequest().user;
+  const user = ctx.switchToHttp().getRequest().user;
+  return { ...user, id: Types.ObjectId.createFromHexString(user.id) };
 });
