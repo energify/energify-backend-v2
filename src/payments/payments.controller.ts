547a84f840888db6d85bc8cdcec8782c4cae1181
Daniel Andrade <dani.andrade@ua.pt>
2021-05-31 15:58:16 +0100

    :sparkles: Added Invoices PDF

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { Types } from 'mongoose';
 import { AuthedUser } from '../auth/decorators/authed-user.decorator';
+import { Public } from '../auth/decorators/public.decorator';
 import { IUser } from '../auth/interfaces/iuser.interface';
 import { ParseObjectIdPipe } from '../common/pipes/parse-objectid.pipe';
 import { CompletePaymentDto } from './dto/complete-payment.dto';
@@ -9,6 +11,17 @@ import { PaymentsService } from './payments.service';
 @Controller('payments')
 export class PaymentsController {
   constructor(private paymentsService: PaymentsService) {}
+
+  @Get(':paymentId')
+  @Public() //TODO
+  async generatePdf(
+    @Res() res: Response,
+    @Param('paymentId', ParseObjectIdPipe) paymentId: Types.ObjectId,
+    @AuthedUser() user: IUser,
+  ) {
+    const stream = await this.paymentsService.generatePdfById(paymentId, user);
+    return stream.pipe(res);
+  }
 
   @Get('me')
   async findByAuthedUser(@AuthedUser() user: IUser) {
