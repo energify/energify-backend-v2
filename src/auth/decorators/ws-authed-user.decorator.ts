5ef361da1b4476b568b3b893e9f8830112f9fe5b
Vasco Sousa <myemailvasco@gmail.com>
2021-05-31 16:02:24 +0100

    :recycle: ObjectId in IUser interface (ws)

--- src/auth/decorators/ws-authed-user.decorator.ts
+++ src/auth/decorators/ws-authed-user.decorator.ts
@@ -1,6 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Types } from 'mongoose';
 import { Socket } from 'socket.io';
 
 export const WsAuthedUser = createParamDecorator((_, ctx: ExecutionContext) => {
-  return (ctx.switchToWs().getClient<Socket>().handshake as any).user;
+  const user = (ctx.switchToWs().getClient<Socket>().handshake as any).user;
+  return { ...user, id: Types.ObjectId.createFromHexString(user.id) };
 });
